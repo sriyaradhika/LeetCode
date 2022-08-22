106106752cd271c81789ce4d53f7c9a50f6f07d9
Nallam Sriya Radhika <91793895+sriyaradhika@users.noreply.github.com>
2022-08-22 16:05:10 +0530

    Time: 154 ms (47.92%), Space: 51.2 MB (69.95%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        unordered_set<int>s;
+        s.insert(nums[0]);
         for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
+            if(s.find(nums[i])==s.end())
+                s.insert(nums[i]);
+            else
                 return true;
-            }
         }
         return false;
     }
+    //     sort(nums.begin(),nums.end());
+    //     for(int i=1;i<nums.size();i++){
+    //         if(nums[i]==nums[i-1]){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 };
