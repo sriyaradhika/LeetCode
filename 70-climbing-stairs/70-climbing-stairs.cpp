dfce51d38119d6cf9202ebfdb116c5a6f03d609d
Nallam Sriya Radhika <91793895+sriyaradhika@users.noreply.github.com>
2022-08-25 14:57:40 +0530

    Time: 6 ms (5.98%), Space: 6.2 MB (15.85%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climb(int n,vector<int>&dp) {
+        if(n<2)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+    }
+     int climbStairs(int n){
+         vector<int>dp(n+1,-1);
+         return climb(n,dp);
+     }
+};
