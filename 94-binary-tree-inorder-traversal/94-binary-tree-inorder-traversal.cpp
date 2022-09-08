43ebdb6a8faf9f32a861c70ba028fcfbb9830fa2
Nallam Sriya Radhika <91793895+sriyaradhika@users.noreply.github.com>
2022-09-08 14:07:02 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (75.10%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,30 +11,42 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>inorder;//we have took inorder vector
-        TreeNode* cur=root;//so that root cannot get destroyed
-        while(cur!=NULL){
-            if(cur->left==NULL){
-                inorder.push_back(cur->val);
-                cur=cur->right;
-            }
-            else{
-                TreeNode* prev=cur->left;
-                while(prev->right && prev->right!=cur){
-                    prev=prev->right;
-                }
-                if(prev->right==NULL){
-                    prev->right=cur;
-                    cur=cur->left;
-                }
-                else{
-                    prev->right=NULL;
-                    inorder.push_back(cur->val);
-                    cur=cur->right;
-                }
-            }
+    void help(TreeNode* cur,vector<int>&in){
+        if(cur==NULL){
+            return;
         }
-        return inorder;
+        help(cur->left,in);
+        in.push_back(cur->val);
+        help(cur->right,in);
     }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>in;
+        help(root,in);
+        return in;
+    }
+    //     vector<int>inorder;//we have took inorder vector
+    //     TreeNode* cur=root;//so that root cannot get destroyed
+    //     while(cur!=NULL){
+    //         if(cur->left==NULL){
+    //             inorder.push_back(cur->val);
+    //             cur=cur->right;
+    //         }
+    //         else{
+    //             TreeNode* prev=cur->left;
+    //             while(prev->right && prev->right!=cur){
+    //                 prev=prev->right;
+    //             }
+    //             if(prev->right==NULL){
+    //                 prev->right=cur;
+    //                 cur=cur->left;
+    //             }
+    //             else{
+    //                 prev->right=NULL;
+    //                 inorder.push_back(cur->val);
+    //                 cur=cur->right;
+    //             }
+    //         }
+    //     }
+    //     return inorder;
+    // }
 };
