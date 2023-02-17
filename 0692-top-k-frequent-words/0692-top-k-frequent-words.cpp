5688e039ebb2b0b1672775ca7032f6e79ea7fec9
Nallam Sriya Radhika <91793895+sriyaradhika@users.noreply.github.com>
2023-02-17 10:19:03 +0530

    Time: 14 ms (64.24%), Space: 12.6 MB (61.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
+    struct cmp{
+        bool operator()(const pair<int,string>&a,const pair<int,string>&b){
+            if(a.first!=b.first){
+                return a.first>b.first;
+            }
+            else{
+                return a.second<b.second;
+            }
+        }
+    };
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> hashmap;
-        for(string& word : words) {
-            hashmap[word] += 1;
+        unordered_map<string,int>mp;
+        for(auto &st:words){
+            mp[st]+=1;
         }
-        priority_queue<pair<int, string>, vector<pair<int, string>>, MyComp> pq;
-        for(auto it = hashmap.begin(); it != hashmap.end(); ++it) {
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > k) pq.pop();
+        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp>pq;
+        for(auto it=mp.begin();it!=mp.end();++it){
+            pq.push(make_pair(it->second,it->first));
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
-        vector<string> res;
-        while(!pq.empty()) {
-            res.insert(res.begin(), pq.top().second);
+    vector<string>res;
+        while(!pq.empty()){
+            res.insert(res.begin(),pq.top().second);
             pq.pop();
         }
         return res;
     }
-private:
-    struct MyComp {
-        bool operator() (const pair<int, string>& a, const pair<int, string>& b) {
-            if(a.first != b.first) {
-                return a.first > b.first;
-            }
-            else {
-                return a.second < b.second;
-            }
-        }
-    };
+    
 };
